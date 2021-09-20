dbf2864276a31e6b387d0104a897e7a869f28640
mariklim <maria.klimenko889@gmail.com>
2021-09-20 15:39:22 +0200

    add btn function click "add new to do"

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 var app = new Vue({
     el: '#root',
     data: {
-      
+      todoList:[
+        ],
+       newToDoThing:"",
     },
     methods: {
-
+        addNewTodo: function(){
+            this.todoList.push(this.newToDoThing)
+        }
     },
-  })
+})
